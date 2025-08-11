07ab9ec30e007acc40de7a200a690b0704132114
송원준 <106801458+WJwonjun@users.noreply.github.com>
2025-08-11 18:00:21 +0900

    25/08/11

---
+++ gold/11404.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+N = int(input())
+M = int(input())
+graph = [[INF] * (N + 1) for _ in range(N + 1)] 
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        if i==j:
+            graph[i][j]=0
+
+for _ in range(M):
+    a,b,c = map(int,input().split())
+    graph[a][b] = min(c, graph[a][b])
+
+for k in range(1,N+1):
+    for a in range(1,N+1):
+        for b in range(1,N+1):
+            graph[a][b] = min(graph[a][b],graph[a][k]+graph[k][b])
+
+for a in range(1,N+1):
+    for b in range(1,N+1):
+        if graph[a][b]==INF :
+            print("0", end=' ')
+        else:
+            print(graph[a][b],end=" ")
+    print()               
+                
