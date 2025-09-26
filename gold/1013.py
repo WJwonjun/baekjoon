bad0d77e94e0e3dd7dfc7013b1d08bb6ae9600bc
송원준 <106801458+WJwonjun@users.noreply.github.com>
2025-09-26 13:09:22 +0900

    25/09/26

---
+++ gold/1013.py
@@ -0,0 +1,58 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    nums = list(map(int,input().strip()))
+    #print(nums)
+
+    zero_cnt = 0
+    one_cnt = 0
+
+    while nums:
+        cur = nums.pop()
+
+        if cur==0:
+            if len(nums)==0:
+                if one_cnt==1 and zero_cnt==0:
+                    print("YES")
+                    break
+                else:
+                    print("NO")
+                    break
+
+            else:
+                    zero_cnt+=1
+
+
+
+        elif cur==1:
+            if zero_cnt==0:
+                if len(nums)==0:
+                    print("NO")
+                    break
+
+                one_cnt+=1
+
+            elif zero_cnt==1:
+                if one_cnt==1:
+                    if len(nums)==0:
+                        print("NO")
+                        break
+                    zero_cnt,one_cnt = 0,1
+                else:
+                    print("NO")
+                    break
+
+            elif zero_cnt>=2:
+                if one_cnt>=1:
+                    zero_cnt,one_cnt=0,0
+                else:
+                    print("NO")
+                    break
+        #print(cur,nums,zero_cnt,one_cnt)
+
+    else: 
+        print("YES")
+
+            
