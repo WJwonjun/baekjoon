e35e594aadba8b5afdbd0726cfcfa91c08abf0ba
WJwonjun <106801458+WJwonjun@users.noreply.github.com>
2025-10-01 11:25:36 +0900

    25/10/01

---
+++ gold/1052.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+
+if N <= K:  # 이미 물병 개수가 조건 이하
+    print(0)
+else:
+    cnt = 0
+    while bin(N).count("1") > K:
+        # 가장 작은 1의 자리 물병을 하나 더 산다고 생각
+        add = (N & -N)  # N에서 가장 작은 2^k 값
+        N += add
+        cnt += add
+    print(cnt)
