7daf6f52052bf1ff8d08794961e4be9b2ee175bf
WJwonjun <106801458+WJwonjun@users.noreply.github.com>
2025-10-18 13:59:33 +0900

    25/10/18

---
+++ gold/1062.py
@@ -0,0 +1,32 @@
+
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+def word2bit(word):
+    bit = 0
+    for char in word:
+        bit = bit | (1 << ord(char) - ord('a'))
+    return bit
+
+N,K = list(map(int, input().rstrip().split()))
+essential = 532741
+words = [list(input().rstrip()) for _ in range(N)]
+bits = list(map(word2bit, words))
+
+if K<5:
+    print(0)
+elif K>=26:
+    print(N)
+
+else:
+    alphabet = [1 << i for i in range(26) if not (essential & 1 << i)]
+    answer = 0
+    for comb in combinations(alphabet,K-5):
+        know = sum(comb)|essential
+        count = 0
+        for bit in bits:
+            if bit&know==bit:
+                count+=1
+        answer = max(answer,count)
+    print(answer)
--- gold/1263.py
+++ gold/1263.py
@@ -6,9 +6,10 @@ nums = []
 for _ in range(N):
     T,S = map(int,input().split())
     nums.append((T,S))
-nums.sort(key = lambda x: (x[1],x[0])) #급하고 오래 걸리는 순 정렬
-#print(nums)
-
+nums.sort(key = lambda x: (x[1],x[0])) #급하고 일찍 끝나는 순 정렬
+#start 기준으로 정렬할 경우 시작은 빨리 해야하는데 갭차이 정확하게 반영 불가능
+# ex. 지금 5초인데 8초에는 시작해야 하고 10초 걸리는 task랑 9초에는 시작해야 하고 2초 걸리는 task 중 앞에걸 먼저 실행해버림
+# (10,8):(2,9) vs (10,18):(2,11) 후자가 맞음
 t=0
 
 while True:
