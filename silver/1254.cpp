5203b590895e20ed5751a0cab730d9fe9e9f8d99
WJwonjun <106801458+WJwonjun@users.noreply.github.com>
2025-12-28 01:30:17 +0900

    25/12/28

--- silver/1254.cpp
+++ silver/1254.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-
+#include <algorithm>
+#include <cstring>
 using namespace std;
+
+bool isPal(const string& s, int start){
+    int l = start, r = s.size()-1;
+    while (l < r) {
+        if (s[l++] != s[r--]) return false;
+    }
+    return true;
+}
+
 int main(){
-    char str [50];
-    
-    cin >> str;
-    cout << str;
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    for(int i=0;i<n;i++){
+        if(isPal(s,i)){
+            cout << n+i << endl;
+            break;
+        }
+    }
 }
